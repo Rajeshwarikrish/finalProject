a0a9eb7180f6edf9a98c5dc550938a9ab747e9ab
rajeshwari krishnamurthy <rk633@afsconnect1.njit.edu>
2017-12-07 03:22:39 -0500

    changed database connection

--- mvc/database.php
+++ mvc/database.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DATABASE', 'sd686');
-define('USERNAME', 'sd686');
-define('PASSWORD', 'ZOm1EN5l3');
+define('DATABASE', 'rk633');
+define('USERNAME', 'rk6330');
+define('PASSWORD', 'LKVWAEKo');
 define('CONNECTION', 'sql1.njit.edu');
