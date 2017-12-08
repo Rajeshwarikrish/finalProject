415536c0bf6229d87a5172d8863bbdd0aec9763b
rajeshwari krishnamurthy <rk633@afsconnect1.njit.edu>
2017-12-08 02:29:13 -0500

    modified core-database-model

--- mvc/core/database/model.php
+++ mvc/core/database/model.php
@@ -1,11 +1,14 @@
 <?php
 namespace database;
-
+use http\controller;
 abstract class model
 {
-
     public function save()
     {
+        if($this->validate() == FALSE) {
+            echo 'failed validation';
+            exit;
+        }
         if ($this->id != '') {
             $sql = $this->update();
         } else {
@@ -15,31 +18,20 @@ abstract class model
         $db = dbConn::getConnection();
         $statement = $db->prepare($sql);
         $array = get_object_vars($this);
-
         if ($INSERT == TRUE) {
-
             unset($array['id']);
-
         }
-
         foreach (array_flip($array) as $key => $value) {
             $statement->bindParam(":$value", $this->$value);
         }
         $statement->execute();
         if ($INSERT == TRUE) {
-
             $this->id = $db->lastInsertId();
-
         }
-
-
         return $this->id;
-
-    }
-
+        }
     private function insert()
     {
-
         $modelName = static::$modelName;
         $tableName = $modelName::getTablename();
         $array = get_object_vars($this);
@@ -49,14 +41,14 @@ abstract class model
         $sql = 'INSERT INTO ' . $tableName . ' (' . $columnString . ') VALUES (' . $valueString . ')';
         return $sql;
     }
-
+    public function validate() {
+        return TRUE;
+    }
     private function update()
     {
-
         $modelName = static::$modelName;
         $tableName = $modelName::getTablename();
         $array = get_object_vars($this);
-
         $comma = " ";
         $sql = 'UPDATE ' . $tableName . ' SET ';
         foreach ($array as $key => $value) {
@@ -67,9 +59,7 @@ abstract class model
         }
         $sql .= ' WHERE id=' . $this->id;
         return $sql;
-
     }
-
     public function delete()
     {
         $db = dbConn::getConnection();
@@ -80,5 +70,4 @@ abstract class model
         $statement->execute();
     }
 }
-
 ?>
